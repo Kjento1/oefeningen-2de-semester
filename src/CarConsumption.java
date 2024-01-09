bc780fd554fe9f282b1f2325d4b8c9fd23297e93
Kjento <kjentomod@gmail.com>
2024-01-09 12:32:18 +0100

    Oefeningen

---
+++ src/CarConsumption.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class CarConsumption {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("geef het aantal kilometers in: ");
+        double kilometers = scanner.nextDouble();
+        System.out.print("geef de hoeveelheid liter in: ");
+        double liters = scanner.nextDouble();
+
+        double consumption = (liters / kilometers) * 100;
+        System.out.print("Je verbruik per 100km is " + consumption);
+
+    }
+}
--- src/Permutation.java
+++ src/Permutation.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Permutation {
+public class Permutation {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 9;
+        int c = 97;
+        int temp = a;
+        a = b;
+        b = c;
+        c = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
 }
